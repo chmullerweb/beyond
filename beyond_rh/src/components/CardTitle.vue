82faac85d06ec8764f1cfdb869d7c24ce8ad7dc9
chmullerweb <ch.muller.web@gmail.com>
2020-08-18 11:56:02 +0200

    Création CardTitle

---
+++ beyond_rh/src/components/CardTitle.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="cardTitle container">
+        <div class="row justify-content-md-center">
+            <div class="col">
+            <h1 class="claim">Recrutez autrement, Recrutez sportivement</h1>
+            <p>Gummies jujubes carrot cake tiramisu. Chocolate cake dragée soufflé gummi bears candy croissant croissant. Chupa chups sesame snaps cotton candy cake oat cake. Icing bonbon powder topping sugar plum sugar plum apple pie. Cupcake oat cake pudding jelly beans jelly beans soufflé jelly-o. </p>
+            <Bouton></Bouton>
+            <button class="btn btn-info">Comment ça marche ?</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Bouton from './Bouton.vue';
+
+export default {
+  name: 'CardTitle',
+  components: {Bouton},
+
+};
+</script>
+
+<style>
+
+.cardTitle{
+    border: 2px blue solid; 
+    margin: 1.5rem auto;
+}
+
+.claim{
+    padding: 2rem 0rem 1.3rem 0rem;
+}
+
+
+</style>
---
+++ beyond_rh/src/pages/landing.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="">
+    <CardTitle/>
+    </div>
+</template>
+
+<script>
+import CardTitle from "../components/CardTitle";
+
+
+export default {
+  name: 'Landing',
+  components: {CardTitle},
+//   data() {
+//     return {
+//       users: membres,
+//     }
+//   },
+//   props: {
+//         identifiantClient: Number,
+//         nom: {
+//             type: String,
+//             default: ""
+//         },
+//         prenom: {
+//             type: String,
+//             default: ""
+//         },
+//         adresse: String,
+//         email: {
+//             type: String,
+//             default: ""
+//         } 
+//   },
+//   computed: {
+//       fullName() { return this.firstname + this.lastname }
+//   },
+//   methods: {
+//       showExo1() {
+//           this.$router.push({path: `/exo-1`})
+//       },
+//       showAnne() {
+//           this.$router.push({path: `/users/3`})
+//       },
+//   },
+};
+</script>
