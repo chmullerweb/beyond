193c4927f5b525de4b89d439c288d305b513a36a
soraya-berguiga <soraya.berguiga05@gmail.com>
2020-08-18 21:55:45 +0200

    carousel picture

---
+++ beyond_rh/src/components/Caroussel.vue
@@ -0,0 +1,79 @@
+<!-- template avec width à modif -->
+<template>
+  <div class="CarousselPicture">
+    <b-carousel
+      class="Caroussel"
+      v-model="slide"
+      :interval="3000"
+      controls
+      indicators
+      background="#ccc"
+      img-width="640"
+      img-height="420"
+      style="text-shadow: 1px 1px 2px #000;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+
+      <b-carousel-slide>
+        <template v-slot:img>
+          <img
+            class="d-block img-fluid w-100"
+            width="640"
+            height="420"
+            src="../assets/meet.jpg"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
+
+      <b-carousel-slide>
+        <template v-slot:img>
+          <img
+            class="d-block img-fluid w-100"
+            width="640"
+            height="420"
+            src="../assets/meeting.jpg"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
+
+      <b-carousel-slide>
+        <template v-slot:img>
+          <img
+            class="d-block img-fluid w-100"
+            width="640"
+            height="420"
+            src="../assets/work.jpg"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+  import Caroussel from './Caroussel.vue';
+
+    export default {
+      name: 'CarousselPicture',
+      component: {Caroussel},
+
+      data() {
+        return {
+          slide: 0,
+          sliding: null
+        }
+      },
+      methods: {
+        onSlideStart() {
+          this.sliding = true
+        },
+        onSlideEnd() {
+          this.sliding = false
+        }
+      }
+    }
+</script>
