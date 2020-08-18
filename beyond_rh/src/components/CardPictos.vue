c0f63e9ff1fc7cfc5ae5ee9c4e5c204da3d38c7a
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-18 16:50:58 +0200

    Update CardPictos.vue

---
+++ beyond_rh/src/components/CardPictos.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div class="cardPictos">
+
+    <div class="card bg-dark text-white">
+      <img src="..." class="card-img" alt="...">
+      <div class="card-img-overlay">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+ </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
