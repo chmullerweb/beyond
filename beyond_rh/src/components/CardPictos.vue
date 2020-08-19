181024dc33afa199a807f078abfef03e302c883d
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-19 12:04:52 +0200

    done

--- beyond_rh/src/components/CardPictos.vue
+++ beyond_rh/src/components/CardPictos.vue
@@ -1,26 +1,29 @@
 <template>
 
-  <div class="cardPictos">
+  <div class="cardPictos container-fluid">
 
-    <div class="card bg-dark text-white">
-      <img src="..." class="card-img" alt="...">
+    <div class="card text-white">
+        <img src="" class="card-img" alt="">
       <div class="card-img-overlay">
+        <i class="fas fa-microscope"></i>
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
 
-    <div class="card bg-dark text-white">
-      <img src="..." class="card-img" alt="...">
+    <div class="card text-white">
+        <img src="" class="card-img" alt="">
       <div class="card-img-overlay">
+        <i class="fas fa-trophy"></i>
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
 
-    <div class="card bg-dark text-white">
-      <img src="" class="card-img" alt="">
+    <div class="card text-white">
+        <img src="" class="card-img" alt="">
       <div class="card-img-overlay">
+        <i class="far fa-clock"></i>
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
@@ -37,9 +40,20 @@ export default {
 </script>
 
 <style lang="css" scoped>
-.cardPictos{
-  display: flex;
-  flex-direction: row;
-  background-color: gold;
+
+.card{
+  width: 25rem;
+  height: 20rem;
+  background-color: #454545;
+  display: inline-grid;
+  border-radius: 0!important;
 }
+
+i{
+  font-size: 7rem;
+  margin: 1.5rem;
+}
+/* .cardPictos{
+  width: 100%;
+} */
 </style>
