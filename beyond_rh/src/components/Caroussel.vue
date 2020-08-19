301aa5f2ce5f20979da0c3cb8810683f0356b542
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-19 19:14:26 +0200

    maj

--- beyond_rh/src/components/Caroussel.vue
+++ beyond_rh/src/components/Caroussel.vue
@@ -1,5 +1,3 @@
-<!-- template avec width à modif -->
-
 <template>
   <div class="CarousselPicture caroussel">
     <b-carousel
@@ -9,116 +7,73 @@
       controls
       indicators
       background="#ccc"
+      img-width="1024"
+      img-height="480"
       style="text-shadow: 1px 1px 2px #000;"
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
-
       <b-carousel-slide>
         <template v-slot:img>
           <img
-            class="d-block img-fluid wp-100"
+            class="img-fluid w-100"
+            width="1024"
+            height="480"
             src="../assets/meet.jpg"
             alt="image slot"
-            img-width = "30" img-height = "10"
           >
         </template>
       </b-carousel-slide>
-
       <b-carousel-slide>
         <template v-slot:img>
           <img
-            class="d-block img-fluid w-100"
+            class="img-fluid w-100"
+            width="1024"
+            height="480"
             src="../assets/meeting.jpg"
             alt="image slot"
-            img-width = "30" img-height = "10"
-
           >
         </template>
-        <h1>COCO</h1>
       </b-carousel-slide>
-
       <b-carousel-slide>
         <template v-slot:img>
           <img
-            class="d-block img-fluid w-100"
+            class="img-fluid w-100"
+            width="640"
+            height="420"
             src="../assets/work.jpg"
             alt="image slot"
-            img-width = "30" img-height = "10">
+          >
         </template>
       </b-carousel-slide>
     </b-carousel>
   </div>
 </template>
-
-
-
-
-
-
-<!--
-<template>
-  <div class="CarousselPicture caroussel">
-    <b-carousel
-      class="Caroussel"
-      v-model="slide"
-      :interval="3000"
-      controls
-      indicators
-      background="#ccc"
-      style="text-shadow: 1px 1px 2px #000;"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
-    >
-
-      <b-carousel-slide>
-        <template v-slot:img>
-          <img
-            class="d-block img-fluid wp-100"
-            src="../assets/meet.jpg"
-            alt="image slot"
-            img-width = "500" img-height = "200"
-          >
-        </template>
-      </b-carousel-slide>
-
-      <b-carousel-slide>
-        <template v-slot:img>
-          <img
-            class="d-block img-fluid"
-            src="../assets/meeting.jpg"
-            alt="image slot"
-          >
-        </template>
-        <h1>COCO</h1>
-      </b-carousel-slide>
-
-      <b-carousel-slide>
-        <template v-slot:img>
-          <img
-            class="d-block img-fluid"
-            src="../assets/work.jpg"
-            alt="image slot">
-        </template>
-      </b-carousel-slide>
-    </b-carousel>
-  </div>
-</template> -->
-
 <script>
   import Caroussel from './Caroussel.vue';
-
     export default {
       name: 'Caroussel',
       component: {Caroussel},
-
+      data() {
+        return {
+          slide: 0,
+          sliding: null
+        }
+      },
+      methods: {
+        onSlideStart() {
+          this.sliding = true
+        },
+        onSlideEnd() {
+          this.sliding = false
+        }
+      }
     }
 </script>
 
 <style lang="css" scoped>
 
-.carousselPicture
-{
-  width: 200px !important
+.w-100{
+  width: 40%!important;
 }
 </style>
