1de9656eb6f967670e3caeddd7cdaafcf82090d2
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-19 12:16:48 +0200

    first commit

---
+++ beyond_rh/src/components/CardHome.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="cardHome container-fluid">
+      <div class="card text-white">
+          <img src="" class="card-img" alt="">
+        <div class="card-img-overlay">
+          <i class="fas fa-microscope"></i>
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import Bouton from './Bouton.vue';
+
+export default {
+  name: 'CardHome',
+  components: {Bouton},
+
+};
+</script>
+
+<style>
+
+.cardHome{
+    margin: 1.5rem auto;
+}
+
+.claim{
+    padding: 2rem 0rem 1.3rem 0rem;
+}
+
+</style>
