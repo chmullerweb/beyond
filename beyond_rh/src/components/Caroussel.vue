ec1802af6adf7d53580a8d7c25701d8cc28a669b
soraya-berguiga <soraya.berguiga05@gmail.com>
2020-08-19 17:40:00 +0200

    ok

--- beyond_rh/src/components/Caroussel.vue
+++ beyond_rh/src/components/Caroussel.vue
@@ -1,4 +1,5 @@
 <!-- template avec width à modif -->
+
 <template>
   <div class="CarousselPicture caroussel">
     <b-carousel
@@ -16,9 +17,67 @@
       <b-carousel-slide>
         <template v-slot:img>
           <img
-            class="d-block img-fluid"
+            class="d-block img-fluid wp-100"
             src="../assets/meet.jpg"
             alt="image slot"
+            img-width = "30" img-height = "10"
+          >
+        </template>
+      </b-carousel-slide>
+
+      <b-carousel-slide>
+        <template v-slot:img>
+          <img
+            class="d-block img-fluid w-100"
+            src="../assets/meeting.jpg"
+            alt="image slot"
+            img-width = "30" img-height = "10"
+
+          >
+        </template>
+        <h1>COCO</h1>
+      </b-carousel-slide>
+
+      <b-carousel-slide>
+        <template v-slot:img>
+          <img
+            class="d-block img-fluid w-100"
+            src="../assets/work.jpg"
+            alt="image slot"
+            img-width = "30" img-height = "10">
+        </template>
+      </b-carousel-slide>
+    </b-carousel>
+  </div>
+</template>
+
+
+
+
+
+
+<!--
+<template>
+  <div class="CarousselPicture caroussel">
+    <b-carousel
+      class="Caroussel"
+      v-model="slide"
+      :interval="3000"
+      controls
+      indicators
+      background="#ccc"
+      style="text-shadow: 1px 1px 2px #000;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+
+      <b-carousel-slide>
+        <template v-slot:img>
+          <img
+            class="d-block img-fluid wp-100"
+            src="../assets/meet.jpg"
+            alt="image slot"
+            img-width = "500" img-height = "200"
           >
         </template>
       </b-carousel-slide>
@@ -44,7 +103,7 @@
       </b-carousel-slide>
     </b-carousel>
   </div>
-</template>
+</template> -->
 
 <script>
   import Caroussel from './Caroussel.vue';
@@ -53,19 +112,13 @@
       name: 'Caroussel',
       component: {Caroussel},
 
-      data() {
-        return {
-          slide: 0,
-          sliding: null
-        }
-      },
-      methods: {
-        onSlideStart() {
-          this.sliding = true
-        },
-        onSlideEnd() {
-          this.sliding = false
-        }
-      }
     }
 </script>
+
+<style lang="css" scoped>
+
+.carousselPicture
+{
+  width: 200px !important
+}
+</style>
