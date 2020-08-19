e29c3c010a6cc91a50cc19638a6f23a2a5ee1c6d
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-19 14:50:39 +0200

    ajout bouton

--- beyond_rh/src/components/CardHome.vue
+++ beyond_rh/src/components/CardHome.vue
@@ -8,6 +8,9 @@
           <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         </div>
       </div>
+
+      <Bouton btn btnInfo><slot name="txtButton">Comment ça marche ?</slot></Bouton>
+      
     </div>
 </template>
 
