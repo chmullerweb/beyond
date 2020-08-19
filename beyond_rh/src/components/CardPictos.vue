43d2a28152b711c0f7355800fd8b4b40c9bf96b2
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-19 14:50:27 +0200

    resolve buug

--- beyond_rh/src/components/CardPictos.vue
+++ beyond_rh/src/components/CardPictos.vue
@@ -3,7 +3,7 @@
   <div class="cardPictos">
 
     <div class="card bg-dark text-white">
-      <img src="..." class="card-img" alt="...">
+      <img src="" class="card-img" alt="">
       <div class="card-img-overlay">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -11,7 +11,7 @@
     </div>
 
     <div class="card bg-dark text-white">
-      <img src="..." class="card-img" alt="...">
+      <img src="" class="card-img" alt="">
       <div class="card-img-overlay">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
