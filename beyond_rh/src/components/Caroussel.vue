1f3415aad5b8cbfe0054f42c6449a5a20de7d8ab
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-19 14:14:49 +0200

    update caroussel

---
+++ beyond_rh/src/components/Caroussel.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="CarousselPicture caroussel">
+    <b-carousel
+      class="Caroussel"
+      v-model="slide"
+      :interval="3000"
+      controls
+      indicators
+      background="#ccc"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #000;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <b-carousel-slide>
+        <template v-slot:img>
+          <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="../assets/meet.jpg"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <template v-slot:img>
+          <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="../assets/meeting.jpg"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <template v-slot:img>
+          <img
+            class="d-block img-fluid w-100"
+            width="640"
+            height="420"
+            src="../assets/work.jpg"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
+    </b-carousel>
+  </div>
+</template>
+<script>
+  import Caroussel from './Caroussel.vue';
+    export default {
+      name: 'Caroussel',
+      component: {Caroussel},
+      data() {
+        return {
+          slide: 0,
+          sliding: null
+        }
+      },
+      methods: {
+        onSlideStart() {
+          this.sliding = true
+        },
+        onSlideEnd() {
+          this.sliding = false
+        }
+      }
+    }
+</script>
