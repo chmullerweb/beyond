5ff0a66e4f71fe75d46c6be03a41f1b194e15404
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-19 17:27:28 +0200

    maj

--- beyond_rh/src/App.vue
+++ beyond_rh/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <button type="button" class="btn btn-danger">coco</button>
+    </div> -->
+    <!-- <button type="button" class="btn btn-danger">coco</button> -->
     <router-view/>
   </div>
 </template>
