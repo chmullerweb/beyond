4c3d2863b7e4f90838cb8ca8cce2f5f048c5d796
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-20 13:30:59 +0200

    ok

--- beyond_rh/src/components/CardTeam.vue
+++ beyond_rh/src/components/CardTeam.vue
@@ -5,10 +5,13 @@
     <div :class="{'cardTeam': cardTeam, 'cardTeamRight': cardTeamRight}">
 
         <div class="col-md-4">
-          <img src="" class="card-img" alt="">
+          <!-- <img src="" class="card-img" alt=""> -->
+          <div class="round">
+            <p class="number">1</p>
+          </div>
         </div>
 
-        <div class="card-body col-md-8">
+        <div class="card-body col-md-6">
           <h5 class="card-title team">Card title</h5>
           <p class="card-text team">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         </div>
@@ -48,9 +51,31 @@ export default {
 
 .cardTeamRight{
   display: flex;
-  flex-direction: row!important;
+  flex-direction: row-reverse!important;
   background-color: #A37871;
-  padding: 1.5rem auto;
+  /* padding: 1.5rem auto; */
+}
+
+.number{
+  font-family: 'Alata';
+  font-size: 3rem;
+}
+
+.round{
+  border: 0.3rem solid grey;
+  border-radius: 75px;;
+  padding: 0.2rem;
+  width: 8rem;
+  height: 8rem;
+}
+
+
+.col-md-4{
+  margin: 1rem !important;
+}
+
+.team{
+  font-family: 'Alata';
 }
 
 .card-title.team{
--- beyond_rh/src/pages/landing.vue
+++ beyond_rh/src/pages/landing.vue
@@ -1,11 +1,9 @@
 <template>
     <div class="">
-    <HeaderNavbar/>
     <CardHome/>
     <CardTitle/>
     <CardPictos/>
     <CardConcept/>
-    <Footer/>
     </div>
 </template>
 
