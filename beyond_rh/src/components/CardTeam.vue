2f634e179dbba538cbdbe4dae241476666294e91
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-20 10:36:58 +0200

    first commit

---
+++ beyond_rh/src/components/CardTeam.vue
@@ -0,0 +1,61 @@
+<template>
+
+  <div>
+
+    <div :class="{'cardTeam': cardTeam, 'cardTeamRight': cardTeamRight}">
+
+        <div class="col-md-4">
+          <img src="" class="card-img" alt="">
+        </div>
+
+        <div class="card-body col-md-8">
+          <h5 class="card-title team">Card title</h5>
+          <p class="card-text team">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+
+    </div>
+
+  </div>
+
+</template>
+
+
+<script>
+
+export default {
+ name: 'CardTeam',
+
+ props:{
+   cardTeam:{
+       type: Boolean,
+       default: true,
+   },
+   cardTeamRight:{
+       type: Boolean,
+       default: false,
+   },
+ }
+};
+
+</script>
+
+<style lang="css" scoped>
+
+.cardTeam{
+    margin: 1.5rem auto;
+    background-color: lightBlue;
+}
+
+.team{
+    padding: 2rem 2rem 1.3rem 4rem;
+}
+
+.cardTeamRight{
+  display: flex;
+  flex-direction: row!important;
+  background-color: lightSalmon;
+  margin: 1.5rem auto;
+  padding: 1.5rem auto;
+}
+
+</style>
---
+++ beyond_rh/src/pages/concept.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <div class="">
+
+      <Caroussel/>
+      <CardTeam cardTeam/>
+      <CardTeam cardTeamRight/>
+      <CardTeam cardTeam/>
+
+    </div>
+
+</template>
+
+<script>
+import CardTeam from "../components/CardTeam";
+import Caroussel from "../components/Caroussel";
+
+export default {
+  name: 'Concept',
+  components: {CardTeam, Caroussel},
+};
+</script>
