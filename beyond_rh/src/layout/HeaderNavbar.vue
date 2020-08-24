69d3fc0018acf75e86b87f48beaca8d379de64a3
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-24 10:35:10 +0200

    maj

--- beyond_rh/src/layout/HeaderNavbar.vue
+++ beyond_rh/src/layout/HeaderNavbar.vue
@@ -1,45 +1,20 @@
 <template>
-  
-  <!-- <div> -->
-    <!-- <ul class="nav justify-content-end"> -->
-      <!-- <li class="nav-item">
-        <a class="nav-link active" href="#">Home</a>
-      </li> -->
-      <!-- <li class="nav-item"> -->
-        <!-- <a class="nav-link" href="#">Concept</a> -->
-      <!-- </li> -->
-      <!-- <li class="nav-item"> -->
-        <!-- <a class="nav-link" href="#">Qui sommes-nous ?</a> -->
-      <!-- </li> -->
-      <!-- <li class="nav-item"> -->
-        <!-- <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Nous contacter</a> -->
-      <!-- </li> -->
-    <!-- </ul> -->
-  <!-- </div> -->
-
   <div>
     <ul class="nav justify-content-end">
-
       <li class="nav-item">
         <router-link to="/Landing" class="nav-link active">Home</router-link>
       </li>
-
       <li class="nav-item">
         <router-link to="/Concept" class="nav-link active">Concept</router-link>
       </li>
-
       <li class="nav-item">
         <router-link to="/Offre" class="nav-link active">Qui sommes-nous ?</router-link>
       </li>
-
       <li class="nav-item">
-        <!-- <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Nous contacter</a> -->
         <router-link to="/ContactPage" class="nav-link active">Nous contacter</router-link>
       </li>
-
     </ul>
   </div>
-
 </template>
 
 <script>
