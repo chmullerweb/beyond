d9ddc1a3515828c8859823ce7dadc8138491aaaa
soraya-berguiga <soraya.berguiga05@gmail.com>
2020-08-25 14:52:43 +0200

    fixed espace beug page concept

--- beyond_rh/src/pages/concept.vue
+++ beyond_rh/src/pages/concept.vue
@@ -37,12 +37,9 @@ Beyond vous propose d'utiliser le sport et la psychologie, en d'autres mots la s
            src="../assets/meet.jpg"
          >
        </template>
-       <template v-slot:title>UTILISATION DE MISES EN SITUATION ​SPORTIVES</template>
-       <template v-slot:text>Des mises en situation pour challenger vos candidats !
-​Utilisez le sports et les mises en situation pour comprendre et anticiper les différentes traits de personnalités de vos candidats.
-Vérifiez les différents softs skills comme la capacité d’adaptation, l'écoute ou le management d'équipe en temps réel !
-Quoi de mieux pour s'assurer d'un bon matching !</template>
-     </CardTeam>
+       <template v-slot:title>UTILISATION DE MISES EN SITUATION SPORTIVES</template>
+       <template v-slot:text>Des mises en situation pour challenger vos candidats! Utilisez le sport et les mises en situation pour comprendre et anticiper les différentes traits de personnalité de vos candidats. Vérifiez les différents softs skills comme la capacité d’adaptation, l'écoute ou le management d'équipe en temps réel !
+Quoi de mieux pour s'assurer d'un bon matching !</template></CardTeam>
 
      <Caroussel>
        <template v-slot:title>L'approche psychologique</template>
@@ -57,8 +54,7 @@ Quoi de mieux pour s'assurer d'un bon matching !</template>
         >
        </template>
        <template v-slot:title>ASSUREZ VOUS UN BON RECRUTEMENT !</template>
-       <template v-slot:text>Le corps est un révélateur formidable !<br>
-​Définissez les éléments de personnalité qui seront primordial chez votre futur collaborateur et testez les de manière scientifique et objective.<br>
+       <template v-slot:text>Le corps est un révélateur formidable !<br> Défininnez les éléments de personnalité qui seront primordial chez votre futur collaborateur et testez les de manière scientifique et objective.<br> -->
 Vous assurez ainsi un recrutement humain et scientifique, qui met au coeur vos valeurs ainsi que le candidat.<br>
 C'est un recrutement Win-Win !</template>
       </CardTeam>
