189ce4c1c4e86965765af92a4f07730384eb5bbc
soraya-berguiga <soraya.berguiga05@gmail.com>
2020-08-25 15:26:15 +0200

    ok caroussel

--- beyond_rh/src/components/Caroussel.vue
+++ beyond_rh/src/components/Caroussel.vue
@@ -122,10 +122,10 @@
       }
     },
     methods: {
-      onSlideStart(slide) {
+      onSlideStart() {
         this.sliding = true
       },
-      onSlideEnd(slide) {
+      onSlideEnd() {
         this.sliding = false
       }
     }
