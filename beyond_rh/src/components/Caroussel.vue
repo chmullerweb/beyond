f13e483e12c49682dc6887f1c3ccb209dd23dc88
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-25 21:17:27 +0200

    maj caroussel

--- beyond_rh/src/components/Caroussel.vue
+++ beyond_rh/src/components/Caroussel.vue
@@ -22,10 +22,37 @@
 
       <!-- Slides with custom text -->
       <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-        <h1>Hello world!</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
+        <h1>UTILISEZ DES CRITÈRES OBJECTIFS ET SCIENTIFIQUES</h1>
+        <p>Supprimez les biais du recrutement !
+  ​          Recruteurs comme candidats, les entretiens demandent une attention
+            particulière. Certains candidats ratent leurs entretiens à cause du
+            stress tandis que certains recruteurs ont des biais dans leur
+            jugement (école, à priori...)
+​            Beyond vous propose d'utiliser le sport et la psychologie, en
+            d'autres mots la science pour choisir votre candidat !
+        </p>
+      </b-carousel-slide>
+
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>UTILISATION DE MISES EN SITUATION ​SPORTIVES</h1>
+        <p>Des mises en situation pour challenger vos candidats !
+           ​Utilisez le sports et les mises en situation pour comprendre et
+           anticiper les différentes traits de personnalités de vos candidats.
+           Vérifiez les différents softs skills comme la capacité d’adaptation,
+           l'écoute ou le management d'équipe en temps réel !
+           Quoi de mieux pour s'assurer d'un bon matching !
+        </p>
+      </b-carousel-slide>
+
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>ASSUREZ VOUS UN BON RECRUTEMENT !</h1>
+        <p>Le corps est un révélateur formidable !
+           Définissez les éléments de personnalité qui seront primordial chez
+           votre futur collaborateur et testez les de manière scientifique et
+           objective.
+           Vous assurez ainsi un recrutement humain et scientifique, qui met
+           au coeur vos valeurs ainsi que le candidat. 
+           C'est un recrutement Win-Win !
         </p>
       </b-carousel-slide>
 
