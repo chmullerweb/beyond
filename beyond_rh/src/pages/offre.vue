a29fae42c4078a431a4717f7e8f2fd2ae9863bcf
chmullerweb <ch.muller.web@gmail.com>
2020-08-26 11:53:14 +0200

    update master fix offre.vue & FormContact.vue

--- beyond_rh/src/pages/offre.vue
+++ beyond_rh/src/pages/offre.vue
@@ -10,10 +10,12 @@
 <script>
 import CardTitle from "../components/CardTitle";
 import Carousseletapes from "../components/Carousseletapes";
-import Bouton from "../components/Bouton";
+//import Bouton from "../components/Bouton";
 
 export default {
   name: 'Offre',
-  components: {Bouton, Carousseletapes, CardTitle},
+  components: {Carousseletapes, CardTitle
+  //,Bouton
+  },
 };
 </script>
