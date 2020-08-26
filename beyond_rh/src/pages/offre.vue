9ba0ecdf2aac573ea96ef4e38f46fde3367bec85
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-26 12:02:18 +0200

    ok

--- beyond_rh/src/pages/offre.vue
+++ beyond_rh/src/pages/offre.vue
@@ -14,14 +14,8 @@ import Carousseletapes from "../components/Carousseletapes";
 
 export default {
   name: 'Offre',
-<<<<<<< HEAD
-  components: {Bouton, Carousseletapes, CardTitle},
-  // components: {Carousseletapes, CardTitle},
-
-=======
   components: {Carousseletapes, CardTitle
   //,Bouton
   },
->>>>>>> a29fae42c4078a431a4717f7e8f2fd2ae9863bcf
 };
 </script>
