177730cf37a2e94ad2211440a340b5c37b0c280e
chmullerweb <ch.muller.web@gmail.com>
2020-08-26 11:13:39 +0200

    Update app.js

--- beyond_rh/app.js
+++ beyond_rh/app.js
@@ -17,7 +17,13 @@ app.engine(
 );
 app.set('view engine', 'handlebars');
 
+app.get('contact', (req, res) => {
+    console.log("tutu")
+})
 
+app.post('/contact', (req, res) => {
+    console.log(req.body)
+})
 
 // Static folder
 app.use('/public', express.static(path.join(__dirname, 'public')));
@@ -28,9 +34,9 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.render('contact');
-});
+// app.get('/', (req, res) => {
+//     res.render('contact');
+// });
 
-app.listen(3000, () => console.log('Server started'));
+app.listen(8080, () => console.log('Server started'));
 
