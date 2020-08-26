e14c8d69fb06068dbee733bcb7029771a83c852b
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-26 19:11:23 +0200

    ok css

--- beyond_rh/src/components/Caroussel.vue
+++ beyond_rh/src/components/Caroussel.vue
@@ -7,8 +7,8 @@
       controls
       indicators
       background="#ababab"
-      img-width="102"
-      img-height="48"
+      img-width="1024"
+      img-height="480"
       style="text-shadow: 1px 1px 2px #333;"
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
