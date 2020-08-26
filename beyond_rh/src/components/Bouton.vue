d3262593db6ae682bf18c0c0c94bea8c0b25aedd
chmullerweb <ch.muller.web@gmail.com>
2020-08-26 18:08:44 +0200

    Update Bouton.vue

--- beyond_rh/src/components/Bouton.vue
+++ beyond_rh/src/components/Bouton.vue
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> master
 <template>
   <div class="bouton_principal">
     <button
       v-on:click="displayForm()"
       type="button"
-<<<<<<< HEAD
-      :class="{ btn: btn, 'btn-dark': btnDark, 'btn-info': btnInfo, 'btn-big': btnBig }"
-=======
       :class="{ btn: btn, 'btn-dark': btnDark, 'btn-info': btnInfo, 'big-btn': bigBtn }"
->>>>>>> master
     >
       <slot>Default Text</slot>
     </button>
@@ -38,17 +29,10 @@ export default {
       type: Boolean,
       default: false,
     },
-<<<<<<< HEAD
     btnBig: {
       type: Boolean,
       default: false,
     }
-=======
-    bigBtn: {
-      type: Boolean,
-      default: false,
-    },
->>>>>>> master
   },
 
   data() {
@@ -58,25 +42,15 @@ export default {
 </script>
 
 <style lang="css" scoped>
-.bouton_principal {
-<<<<<<< HEAD
+.bouton_principal{
   padding: 2rem 0rem;
 }
 
 .btn-big{
-=======
-  margin: 2rem 0rem;
-}
-
-.big-btn{
->>>>>>> master
 width: 50%;
 height: 4rem;
 font-size: 2rem;
 }
 
 </style>
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> master
+
