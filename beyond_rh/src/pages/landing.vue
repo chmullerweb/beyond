1cd25e2af2d13b13e8fde1abf6588351df904bb9
soraya-berguiga <soraya.berguiga05@gmail.com>
2020-08-26 22:09:40 +0200

    texte page landing ok

--- beyond_rh/src/pages/landing.vue
+++ beyond_rh/src/pages/landing.vue
@@ -9,7 +9,7 @@
        <i class="fas fa-microscope"></i>
      </template>
      <template v-slot:claimTitle>Recrutez autrement, Recrutez sportivement</template>
-     <template v-slot:claimTxt>Gummies jujubes carrot cake tiramisu. Chocolate cake dragée soufflé gummi bears candy croissant croissant. Chupa chups sesame snaps cotton candy cake oat cake. Icing bonbon powder topping sugar plum sugar plum apple pie. Cupcake oat cake pudding jelly beans jelly beans soufflé jelly-o.</template>
+     <template v-slot:claimTxt> <p>Des solutions innovantes et personnalisées de recrutement, via des outils d’évaluations conçus par des psychologues du sport.</p> <p>L’objectif est de faire ressortir les points forts de la personnalité du candidat,  afin de recruter le profil qui correspond à vos attentes.</p></template>
 
     </CardTitle>
 
@@ -21,7 +21,7 @@
        <i class="far fa-clock"></i>
      </template>
      <template v-slot:title>GAIN DE TEMPS</template>
-     <template v-slot:text>En moyenne un recrutement se fait sur 4 à 5 semaines... Soit 175 h à chercher un futur collaborateur, qui parfois n'est pas le bon...</template>
+     <template v-slot:text> Simplifiez vos recutements grace à un procédé pertinent et efficace  </template>
    </CardPictos>
 
     <CardPictos>
@@ -29,7 +29,8 @@
         <i class="fas fa-microscope"></i>
       </template>
       <template v-slot:title>EVALUATION DES SOFTS SKILLS</template>
-      <template v-slot:text>Comment vérifier immédiatement les softs skills d'un futur collaborateur ? Les tests en ligne s'avère parfois décevant...</template>
+      <template v-slot:text>
+        Détecter des talents uniques souvent indétectables lors de recrutement classique </template>
    </CardPictos>
 
    <CardPictos>
@@ -37,7 +38,7 @@
        <i class="fas fa-trophy"></i>
      </template>
      <template v-slot:title>DÉCELER LE BON CANDIDAT</template>
-     <template v-slot:text>Comment déceler le vrai du faux dans le discours des candidats ? Le recrutement doit être objectif et sans biais...</template>
+     <template v-slot:text>Percevez efficament les qualités relationnelles et professionnelles</template>
    </CardPictos>
 
  </div>
