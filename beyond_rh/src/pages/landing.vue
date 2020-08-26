ed225b3490b458e2fe7e90bf0d2a65b9fdde3de8
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-26 17:04:04 +0200

    OK

--- beyond_rh/src/pages/landing.vue
+++ beyond_rh/src/pages/landing.vue
@@ -16,10 +16,18 @@
   <div class="cardPictos container-fluid">
 
     <CardPictos>
+     <template v-slot:icone>
+       <i class="far fa-clock"></i>
+     </template>
+     <template v-slot:title>GAIN DE TEMPS</template>
+     <template v-slot:text>En moyenne un recrutement se fait sur 4 à 5 semaines... Soit 175 h à chercher un futur collaborateur, qui parfois n'est pas le bon...</template>
+   </CardPictos>
+
+    <CardPictos>
       <template v-slot:icone>
         <i class="fas fa-microscope"></i>
       </template>
-      <template v-slot:title>EVALUER LES SOFTS SKILLS</template>
+      <template v-slot:title>EVALUATION DES SOFTS SKILLS</template>
       <template v-slot:text>Comment vérifier immédiatement les softs skills d'un futur collaborateur ? Les tests en ligne s'avère parfois décevant...</template>
    </CardPictos>
 
@@ -30,14 +38,6 @@
      <template v-slot:title>DÉCELER LE BON CANDIDAT</template>
      <template v-slot:text>Comment déceler le vrai du faux dans le discours des candidats ? Le recrutement doit être objectif et sans biais...</template>
    </CardPictos>
-
-   <CardPictos>
-    <template v-slot:icone>
-      <i class="far fa-clock"></i>
-    </template>
-    <template v-slot:title>GAIN DE TEMPS</template>
-    <template v-slot:text>En moyenne un recrutement se fait sur 4 à 5 semaines... Soit 175 h à chercher un futur collaborateur, qui parfois n'est pas le bon...</template>
-  </CardPictos>
 
  </div>
 
