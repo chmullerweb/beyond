5a0dff666875722d27f6b11174e80bb05e2fbb2d
paloma-cattalano <paloma.cattalano@gmail.com>
2020-08-28 17:39:00 +0200

    ok

--- beyond_rh/src/pages/offre.vue
+++ beyond_rh/src/pages/offre.vue
@@ -6,11 +6,12 @@
     <template v-slot:claimTxt>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</template>
     </CardTitle>
     <Carousseletapes/>
-    
+
   </div>
 </template>
 
 <script>
+
 import CardTitle from "../components/CardTitle";
 import Carousseletapes from "../components/Carousseletapes";
 
@@ -22,4 +23,5 @@ export default {
     }
   }
 };
+
 </script>
